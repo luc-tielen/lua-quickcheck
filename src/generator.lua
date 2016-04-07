4e3e526c256ac98fb396ae154e92cf88a88a2c7e
Luc Tielen <luc.tielen@gmail.com>
2016-04-07 22:51:04 +0200

    int() now generates between +- sample_size / 2

--- src/generator.lua
+++ src/generator.lua
@@ -1,18 +1,19 @@
-
+local lqc = require 'src.quickcheck'  -- TODO split off iter amount into config module
 local Gen = {}
 local Gen_mt = { __index = Gen }
 
 function Gen.new(pick_func, shrink_func)
   local Generator = {
     pick_func = pick_func,
-    shrink_func = shrink_func
+    shrink_func = shrink_func,
+    numtests = lqc.iteration_amount
   }
 
   return setmetatable(Generator, Gen_mt)
 end
 
 function Gen:pick()
-  return self.pick_func()
+  return self.pick_func(self.numtests)
 end
 
 function Gen:shrink(prev)
--- src/generators/int.lua
+++ src/generators/int.lua
@@ -1,34 +1,43 @@
 local Gen = require 'src.generator'
 local random = require 'src.random'
 
-local limit = 2 ^ 32  -- TODO figure out upper limit of integers
-
-local function pick(min, max)
+-- Picks a random integer, bounded by min and max. (min <= int <= max)
+local function pick_bounded(min, max)
   local function do_pick()
     return random.between(min, max)
   end
   return do_pick
 end
 
+-- Picks a random integer, uniformy spread between +- sample_size / 2.
+local function pick_uniform(sample_size)
+  local value = sample_size / 2
+  return random.between(value - sample_size, value)
+end
+
+-- Shrinks an integer by dividing it by 2 and rounding towards 0.
 local function shrink(previous)
   if previous == 0 then return 0 end
   if previous > 0 then return math.floor(previous / 2) end
   return math.ceil(previous / 2)
 end
 
+-- Picks an integer between min and max.
 local function integer_between(min, max)
-  return Gen.new(pick(min, max), shrink)
+  return Gen.new(pick_bounded(min, max), shrink)
 end
 
+-- Picks a positive integer between 0 and max.
 local function positive_integer(max)
-  return Gen.new(pick(0, max), shrink)
+  return Gen.new(pick_bounded(0, max), shrink)
 end
 
+-- Picks an integer uniformly chosen between +- sample_size / 2.
 local function integer()
-  local value = limit / 2
-  return Gen.new(pick(value - limit, value), shrink)
+  return Gen.new(pick_uniform, shrink)
 end
 
+-- Creates a new int generator.
 local function new(nr1, nr2)
   if nr1 and nr2 then return integer_between(nr1, nr2) end
   if nr1 then return positive_integer(nr1) end
