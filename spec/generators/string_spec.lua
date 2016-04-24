94b1f7124a3955f9b68d1f1e6948a25bc3d09422
Luc Tielen <luc.tielen@gmail.com>
2016-04-24 15:14:59 +0200

    Added string() and string(size) generators

---
+++ spec/generators/string_spec.lua
@@ -0,0 +1,110 @@
+local random = require 'src.random'
+local str = require 'src.generators.string'
+local r = require 'src.report'
+local property = require 'src.property'
+local lqc = require 'src.quickcheck'
+local reduce = require 'src.functional.reduce'
+
+local function is_string(s)
+  return type(s) == 'string'
+end
+
+local function string_sum(s)
+  local numbers = { string.byte(s, 1, #s) }
+  return reduce(numbers, 0, function(x, acc) return x + acc end)
+end
+
+local function do_setup()
+  random.seed()
+  lqc.properties = {}
+  r.report = function() end
+end
+
+
+describe('string generator module', function()
+  before_each(do_setup)
+
+  describe('pick function', function()
+    it('should pick an arbitrary length string if size not specified', function()
+      local results = {}
+      local spy_check = spy.new(function(x) 
+        table.insert(results, #x)
+        return is_string(x) 
+      end)
+
+      property 'string() should pick an arbitrary sized string' {
+        generators = { str() },
+        check = spy_check
+      }
+      lqc.check()
+      assert.spy(spy_check).was.called(lqc.iteration_amount)
+      
+      -- If all lengths were equal:
+      -- sum of lengths = first element times iteration_amount
+      local sum_lengths = reduce(results, 0, function(x, acc) return x + acc end)
+      assert.not_equal(results[1] * lqc.iteration_amount, sum_lengths)
+    end)
+
+    it('should pick a fixed size string if size is specified', function()
+      local length = 3
+      local spy_check = spy.new(function(x) 
+        return is_string(x) and #x == length
+      end)
+
+      property 'string(len) should pick a fixed length string (size len)' {
+        generators = { str(length) },
+        check = spy_check
+      }
+      lqc.check()
+      assert.spy(spy_check).was.called(lqc.iteration_amount)
+    end)
+  end)
+
+  describe('shrink function', function()
+    it('should shrink to a smaller string if no size specified', function()
+      local generated_value, shrunk_value
+      r.report_failed = function(_, generated_vals, shrunk_vals)
+        generated_value = generated_vals[1]
+        shrunk_value = shrunk_vals[1]
+      end
+
+      property 'string() should shrink to smaller and simpler string' {
+        generators = { str() },
+        check = function(x)
+          return not is_string(x) -- always fails!
+        end
+      }
+
+      for _ = 1, 100 do
+        shrunk_value, generated_value = nil, nil
+        lqc.check()
+        assert.is_true(#shrunk_value <= #generated_value)
+        assert.is_true(string_sum(shrunk_value) <= string_sum(generated_value))
+      end
+    end)
+
+    it('should shrink to a simpler string of same size if size specified', function()
+      local length = 5
+      local generated_value, shrunk_value
+      r.report_failed = function(_, generated_vals, shrunk_vals)
+        generated_value = generated_vals[1]
+        shrunk_value = shrunk_vals[1]
+      end
+
+      property 'string(len) should shrink to simpler string of same size' {
+        generators = { str(length) },
+        check = function(x)
+          return not is_string(x)  -- always fails!
+        end
+      }
+
+      for _ = 1, 100 do
+        generated_value, shrunk_value = nil, nil
+        lqc.check()
+        assert.is_equal(length, #shrunk_value)
+        assert.is_true(string_sum(shrunk_value) <= string_sum(generated_value))
+      end
+    end)
+  end)
+end)
+
---
+++ src/generators/string.lua
@@ -0,0 +1,157 @@
+local random = require 'src.random'
+local Gen = require 'src.generator'
+local char = require 'src.generators.char'
+local char_gen = char()
+
+
+-- NOTE: The shrink algorithms are *heavily* based on triq
+--       https://github.com/krestenkrab/triq
+
+-- Helper function to determine how many items to shrink.
+local function shrink_how_many(length)
+  -- 20% chance more than 1 member is shrunk
+  if random.between(1, 5) == 1 then
+    return random.between(1, length)
+  end
+  
+  return 1
+end
+
+-- Replaces a character in the string 'str' at index 'idx' with 'new_char'.
+local function string_replace_char(str, new_char, idx)
+  local result = {}
+  result[1] = string.sub(str, 1, idx - 1)
+  result[2] = new_char
+  result[3] = string.sub(str, idx + 1)
+  return table.concat(result)
+end
+
+-- Replaces 1 character at a random location in the string, tries up to 100
+-- times if shrink gave back same result.
+local function do_shrink_generic(prev, length, iterations_count)
+  local idx = random.between(1, length)
+  local old_char = string.sub(prev, idx, idx)
+  local new_char = char_gen:shrink(old_char)
+
+  if new_char == old_char and iterations_count ~= 0 then
+    -- Shrink introduced no simpler result, retry at other index.
+    return do_shrink_generic(prev, length, iterations_count - 1)
+  end
+
+  return string_replace_char(prev, new_char, idx)
+end
+
+-- Shrinks an amount of characters in the string.
+local function shrink_generic(str, length, how_many)
+  if how_many ~= 0 then
+    local new_str = do_shrink_generic(str, length, 100)  -- TODO = lqc.shrink_amount
+    return shrink_generic(new_str, length, how_many - 1)
+  end
+
+  return str
+end
+
+-- Allow shrinking of strings 1/5th of the time (length > 0)
+local function should_shrink_smaller(str_len)
+  if str_len == 0 then return false end
+  return random.between(1, 5) == 1
+end
+
+-- Returns a new string with 1 character removed.
+local function shrink_smaller(str, str_len)
+  local idx = random.between(1, str_len)
+  
+  -- Handle edge cases (first or last char)
+  if idx == 1 then
+    return string.sub(str, 2)
+  end
+  if idx == str_len then
+    return string.sub(str, 1, idx - 1)
+  end
+
+  local new_str = {
+    string.sub(str, 1, idx - 1),
+    string.sub(str, idx + 1)
+  }
+  return table.concat(new_str)
+end
+
+-- Generates a string with a specific size.
+local function do_generic_pick(size)
+  local result = {}
+  for _ = 1, size do
+    result[#result + 1] = char_gen:pick()
+  end
+  return table.concat(result)
+end
+
+-- Generates a string with arbitrary length (0 <= size <= numtests).
+local function arbitrary_length_pick(numtests)
+  local size = random.between(0, numtests)
+  return do_generic_pick(size)
+end
+
+-- Shrinks a string to a simpler form (smaller / different chars).
+-- 1. Returns empty strings instantly
+-- 2. Determine if string should be made shorter
+-- 2.1 if true: remove a char
+-- 2.2 otherwise:
+--  * simplify a random amount of characters
+--  * remove a char if simplify did not help
+--  * otherwise return the simplified string
+local function arbitrary_length_shrink(prev)
+  local length = #prev
+  if length == 0 then return prev end  -- handle empty strings
+
+  if should_shrink_smaller(length) then
+    return shrink_smaller(prev, length)
+  end
+
+  local new_str = shrink_generic(prev, length, shrink_how_many(length))
+  if new_str == prev then
+    -- shrinking didn't help, remove an element
+    return shrink_smaller(new_str, length)
+  end
+
+  -- string shrunk succesfully!
+  return new_str
+end
+
+-- Generates a string with a specific size.
+local function specific_length_pick(size)
+  local function do_specific_pick()
+    return do_generic_pick(size)
+  end
+  return do_specific_pick
+end
+
+-- Shrinks a string to a simpler form (only different chars since length is fixed).
+--  * "" -> ""
+--  * non-empty string, shrinks upto max 5 chars of the string
+local function specific_length_shrink(prev)
+  local length = #prev
+  if length == 0 then return prev end  -- handle empty strings
+  return shrink_generic(prev, length, shrink_how_many(length))
+end
+
+-- Generator for a string with an arbitrary size.
+local function arbitrary_length_string()
+  return Gen.new(arbitrary_length_pick, arbitrary_length_shrink)
+end
+
+-- Generator for a string of a specific size.
+local function specific_length_string(size)
+  return Gen.new(specific_length_pick(size), specific_length_shrink)
+end
+
+-- Creates a new ascii string generator
+local function new(size)
+  if not size then
+    return arbitrary_length_string()
+  end
+
+  return specific_length_string(size)
+end
+
+return new
+
