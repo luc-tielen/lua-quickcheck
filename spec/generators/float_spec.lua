babac195bfee721ba17dba8a70915b09444d7b35
Luc Tielen <luc.tielen@gmail.com>
2016-04-24 15:47:00 +0200

    Added float generator

---
+++ spec/generators/float_spec.lua
@@ -0,0 +1,63 @@
+local random = require 'src.random'
+local float = require 'src.generators.float'
+local r = require 'src.report'
+local property = require 'src.property'
+local lqc = require 'src.quickcheck'
+
+local function is_float(value)
+  return type(value) == 'number'
+    and value % 1 ~= 0
+end
+
+-- checks if x is in -1..0 if negative or 0..1 if positive
+local function close_to_zero(x)
+  local is_positive = x >= 0
+  return is_positive and x < 1
+                      or x > -1
+end
+
+local function do_setup()
+  random.seed()
+  lqc.properties = {}
+  r.report = function() end
+end
+
+-- TODO rename to real!
+
+describe('float generator module', function()
+  before_each(do_setup)
+
+  describe('pick function', function()
+    it('should pick a float', function()
+      local spy_check = spy.new(function(x) return is_float(x) end)
+      property 'float() should pick a float' {
+        generators = { float() },
+        check = spy_check
+      }
+      lqc.check()
+      assert.spy(spy_check).was.called(lqc.iteration_amount)
+    end)
+  end)
+
+  describe('shrink function', function()
+    it('should converge to 0.0', function()
+      local shrunk_value
+      r.report_failed = function(_, _, shrunk_vals)
+        shrunk_value = shrunk_vals[1]
+      end
+      property 'float() should converge to 0.0' {
+        generators = { float() },
+        check = function(x) 
+          return not is_float(x) -- always fails!
+        end
+      }
+
+      for _ = 1, 100 do
+        shrunk_value = nil
+        lqc.check()
+        assert.is_true(close_to_zero(shrunk_value))
+      end
+    end)
+  end)
+end)
+
---
+++ src/generators/float.lua
@@ -0,0 +1,22 @@
+local Gen = require 'src.generator'
+
+-- TODO rename to real!
+
+-- Picks a random float (between - numtests / 2 and numtests / 2).
+local function float_pick(numtests)
+  local lower_bound = - numtests / 2
+  local upper_bound = numtests / 2
+  return lower_bound + math.random() * (upper_bound - lower_bound)
+end
+
+-- Shrinks a float
+local function float_shrink(prev)
+  return prev / 2
+end
+
+local function new()
+  return Gen.new(float_pick, float_shrink)
+end
+
+return new
+
