8bc19ecdf09663517bb512b48fab8756a339b0df
Luc Tielen <luc.tielen@gmail.com>
2016-07-06 23:03:40 +0200

    Add docs to Action object

--- src/fsm/action.lua
+++ src/fsm/action.lua
@@ -2,7 +2,9 @@
 local Action = {}
 local Action_mt = { __index = Action }
 
-function Action.new(var, cmd)
+
+-- Creates a new action.
+function Action.new(var, cmd, command_generator)
   if var == nil then
     error 'Need to provide variable to action object!'
   end
@@ -10,10 +12,16 @@ function Action.new(var, cmd)
     error 'Need to provide command to action object!'
   end
 
-  local action = { variable = var, command = cmd }
+  local action = { 
+    variable = var, 
+    command = cmd,
+    cmd_gen = command_generator
+  }
   return setmetatable(action, Action_mt)
 end
 
+
+-- returns a string representation of the action.
 function Action:to_string()
   return '{ set, ' .. self.variable:to_string() .. 
               ', ' .. self.command:to_string() .. ' }'
