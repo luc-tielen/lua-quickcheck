b799da4fb9467162d753d7aab7783d001f071866
Luc Tielen <luc.tielen@kabisa.nl>
2019-05-11 10:04:00 +0200

    Pin nixpkgs to specific commit (19.03)

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,8 +1,9 @@
 let
+  commit = "454eea84a757ca5f733c4ec0f234eba2281c74eb";
   nixpkgs = builtins.fetchTarball {
-    name = "nixos-19.03";
-    url = https://github.com/nixos/nixpkgs/archive/19.03.tar.gz;
-    sha256 = "0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
+    name = "nixpkgs-19.03";
+    url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
+    sha256 = "1k9jbix4w43brqlfmvwy218pf5fbmzsnc08shaww9qfdl1rdlaxy";
   };
   pkgs = import nixpkgs;
 in
